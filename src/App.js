1a1fbf6d3e0b4a36ea51379af0f5b1d1f708a2d4
sukku-tech <cherukupallisukanya808gmail.com>
2024-09-08 14:50:56 +0530

    meals-component-commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
-   <Header/>
+   <Fragment>
+    <Header/>
+    <main>
+      <Meals/>
+    </main>
+
+   </Fragment>
   )
 };
 
